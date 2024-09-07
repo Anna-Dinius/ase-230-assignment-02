5dac1834b7a9045349f4a2099a2f2b3e11902699
Dillon Carpenter <dillwilldwc@gmail.com>
2024-09-07 19:30:45 -0400

    memberDetails should show up now

---
+++ memberDetails.php
@@ -0,0 +1,62 @@
+<?php
+    $members=[
+        [
+            'firstname' => '',
+            'lastname' => '',
+            'profilePicture' => '',
+            'phoneNumber' => '',
+            'email' => "",
+            'linkedin' => "",
+            'github' => "",
+            'website' => "",
+            'desiredJobTitle' =>'',
+            'summary' =>'',
+            'workExperience' =>
+            [
+                [
+                    'title' => '',
+                    'company' => '',
+                    'description' => '',
+                    'achievements' => '',
+                    'technologies' => [
+                        'Test';
+                    ];
+                ],
+            ],
+            'skillsTools' =>[
+
+            ],
+            'others' =>[
+
+            ],
+            'education' =>[
+                [
+                    'schoolDegree' => '',
+                    'time' => "";
+                ];
+            ],
+            'awards' =>[
+                [
+                    'awardName' => '',
+                    'awardDescription' => '';
+                ];
+            ],
+            'languages' =>[
+                [
+                    'language' => '',
+                    'skillLevel' => '';
+                ]
+            ],
+            'interests' =>[
+
+            ],
+            'projects' =>[
+                [
+                    'projectTitle' => '',
+                    'projectDescription' => '',
+                    'link' => "",
+                ];
+            ],
+        ];
+    ];
+?>
